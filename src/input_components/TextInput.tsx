c932b65a9b5316d7f758c620911c6f37dc9d4145
alendri <alendri@allbin>
2018-10-26 13:22:23 +0200

    Added barcode reading functionality to text inputs.

--- src/input_components/TextInput.tsx
+++ src/input_components/TextInput.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import Quagga from 'quagga';
+import barcode from '../barcode';
+import { FaBarcode } from 'react-icons/fa';
 
 export interface TextInputConfig {
     label?: string;
     class_name?: string;
+    barcode?: boolean;
+    barcode_stream_visible: boolean;
 }
 export interface TextInputProps {
     value: string;
@@ -14,6 +19,7 @@ export interface TextInputProps {
 
 class TextInput extends React.Component<TextInputProps, TextInputConfig> {
     container: typeof React.Component;
+    barcode_stream_target: HTMLDivElement|null;
 
     constructor(props) {
         super(props);
@@ -37,26 +43,130 @@ class TextInput extends React.Component<TextInputProps, TextInputConfig> {
                     border-color: ${props => props.theme.colors.brand[2]};
                 }
             }
+            .barcode_btn{
+                width: 20px;
+                height: 20px;
+            }
+            .barcode_stream_target{
+                display: none;
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                z-index: 100000000;
+                &.show {
+                    display: block;
+                }
+            }
         `;
+
+        this.barcode_stream_target = null;
+
+        this.state = {
+            barcode_stream_visible: false
+        };
 
     }
 
+    startBarcodeReading() {
+        if (!this.barcode_stream_target) {
+            return;
+        }
+        let quagga_config = {
+            numOfWorkers: navigator.hardwareConcurrency,
+            locate: true,
+            inputStream: {
+                name: "Live",
+                type: "LiveStream",
+                constraints: {
+                    width: 1280,
+                    height: 720,
+                    facingMode: "environment"
+                },
+                target: this.barcode_stream_target
+            },
+            frequency: 5,
+            decoder: {
+                readers: ["ean_reader"]
+            },
+            locator: {
+                halfSample: true,
+                patchSize: "medium"
+            }
+        };
+
+        Quagga.init(quagga_config, (err) => {
+            if (err) {
+                throw err;
+            }
+
+            Quagga.onDetected(this.detectedCB);
+
+            Quagga.start();
+        });
+    }
+
+    detectedCB(data) {
+        barcode.removeDetectedCB(this.detectedCB);
+        barcode.stop();
+
+        let result = data.codeResult.code;
+        this.props.onChange(result);
+    }
+
+    renderBarcodeBtn(cfg) {
+        if (cfg.barcode !== true || barcode.isEnabled() === false) {
+            return null;
+        }
+
+        let barcode_stream_classes = ["barcode_stream_target"];
+
+        if (this.state.barcode_stream_visible) {
+            barcode_stream_classes.push("show");
+        }
+
+        return (
+            <div
+                className="barcode_btn"
+                onClick={() => {
+                    this.startBarcodeReading();
+                }}
+            >
+                <FaBarcode />
+                <div
+                    className={barcode_stream_classes.join(" ")}
+                    ref={(ref) => { this.barcode_stream_target = ref; }}
+                />
+            </div>
+        );
+    }
+
+
+
     render() {
         let cfg = this.props.config;
-        let class_names = "user_input multi_select_input";
+        let class_names = "user_input text_input";
         if (cfg.class_name) {
             class_names += " " + cfg.class_name;
+        }
+
+        let input_class_name = "full";
+        if (cfg.barcode === true) {
+            input_class_name = "small";
         }
 
         return (
             <this.container className={class_names}>
                 { cfg.label ? <p>{ cfg.label }</p> : null }
                 <input
+                    className={input_class_name}
                     autoFocus={this.props.autofocus || false}
                     type="text"
                     value={this.props.value}
                     onChange={e => this.props.onChange(e.target.value)}
                 />
+                { this.renderBarcodeBtn(cfg) }
             </this.container>
         );
     }
