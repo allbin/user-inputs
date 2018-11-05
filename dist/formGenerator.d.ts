1948fa5c8e023f139b0088251c0725461fd8d978
alendri <alendri@allbin>
2018-11-05 16:16:38 +0100

    Release v5.2.13

--- dist/formGenerator.d.ts
+++ dist/formGenerator.d.ts
@@ -1,2 +1,3 @@
+import { ComponentObject, InputConfig } from './index';
 export declare function getInputForm(default_components: ComponentObject, custom_components: ComponentObject, input_configs: InputConfig[], cb: (any: any) => void): any;
 //# sourceMappingURL=formGenerator.d.ts.map
