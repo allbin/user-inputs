f9fdbb6c98f42f9980502c2772681065b25e8769
alendri <alendri@allbin>
2019-02-01 16:15:42 +0100

    Release v7.1.2

--- dist/module/formGenerator.d.ts
+++ dist/module/formGenerator.d.ts
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { LooseObject } from '.';
-import { FormInputConfigArray, AnyInputConfig } from '.';
+import { FormInputConfigArray, AnyInputConfigWithValue } from '.';
 export interface GeneratedForm {
     component: typeof React.Component;
     reset: () => void;
     resetConfirmClick: () => void;
     getValues: () => LooseObject;
-    setInputConfig: (updated_config: Partial<AnyInputConfig>) => void;
+    setInputConfig: (updated_config: AnyInputConfigWithValue) => void;
 }
 export default function getInputForm(input_configs: FormInputConfigArray, cb?: (values: any) => void): GeneratedForm;
 export declare function validateFormGeneratorInputs(input_configs: FormInputConfigArray, confirmCB?: (value: any) => void): void;
