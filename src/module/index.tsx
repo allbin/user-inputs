e394fe1c86c7c8f79867f08e2fb4bd2a65e7463e
Mathias Djärv <mathias.djarv@gmail.com>
2019-03-04 17:38:53 +0100

    use props booleans to show/hide cancel and confirm buttons in PromptModal

--- src/module/index.tsx
+++ src/module/index.tsx
@@ -257,7 +257,7 @@ export function InputHOC<P extends UserInputProps>(WrappedComponent: React.Compo
             this.confirmCB = confirmCB || null;
             this.cancelCB = cancelCB || null;
 
-            if (cancelCB) {
+            if (prompt_config.show_cancel_btn) {
                 let cancel_config: ButtonImport.ButtonConfig = {
                     label: prompt_config.confirm_button_label || oh.translate('user_input_cancel'),
                     key: "cancel",
@@ -270,15 +270,17 @@ export function InputHOC<P extends UserInputProps>(WrappedComponent: React.Compo
                 inputs.push(cancel_config);
             }
 
-            let confirm_config: ButtonImport.ButtonConfig = {
-                label: prompt_config.confirm_button_label || oh.translate('user_input_confirm'),
-                key: "confirm",
-                type: "confirm",
-                default_value: "",
-                filled: true,
-                big: true
-            };
-            inputs.push(confirm_config);
+            if (prompt_config.show_confirm_btn) {
+                let confirm_config: ButtonImport.ButtonConfig = {
+                    label: prompt_config.confirm_button_label || oh.translate('user_input_confirm'),
+                    key: "confirm",
+                    type: "confirm",
+                    default_value: "",
+                    filled: true,
+                    big: true
+                };
+                inputs.push(confirm_config);
+            }
 
             this.setState({
                 form: formGenerator(inputs, (values: LooseObject) => this.userConfirmedCB(values)),
